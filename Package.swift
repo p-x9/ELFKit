36ebda4e47749d9845047ff93995cf3f5953c24d
p-x9 <50244599+p-x9@users.noreply.github.com>
2024-04-26 16:50:36 +0900

    format

--- Package.swift
+++ Package.swift
@@ -1,23 +1,22 @@
 // swift-tools-version: 5.9
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "ELFKit",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "ELFKit",
-            targets: ["ELFKit"]),
+            targets: ["ELFKit"]
+        ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "ELFKit"),
+            name: "ELFKit"
+        ),
         .testTarget(
             name: "ELFKitTests",
-            dependencies: ["ELFKit"]),
+            dependencies: ["ELFKit"]
+        ),
     ]
 )
--- Tests/ELFKitTests/ELFKitTests.swift
+++ Tests/ELFKitTests/ELFKitTests.swift
@@ -1,12 +1,4 @@
 import XCTest
 @testable import ELFKit
 
-final class ELFKitTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
-
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
-    }
-}
+final class ELFKitTests: XCTestCase {}
