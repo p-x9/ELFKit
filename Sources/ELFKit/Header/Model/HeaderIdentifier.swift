d4da0b5c130a1ed29539e399866e570490f2b4c7
p-x9 <50244599+p-x9@users.noreply.github.com>
2024-04-27 01:08:38 +0900

    Implemented `HeaderIdentifier`

---
+++ Sources/ELFKit/Header/Model/HeaderIdentifier.swift
@@ -0,0 +1,40 @@
+//
+//  HeaderIdentifier.swift
+//
+//
+//  Created by p-x9 on 2024/04/26
+//  
+//
+
+import Foundation
+import ELFKitC
+
+public struct HeaderIdentifier: LayoutWrapper {
+    public typealias Layout = (UInt8, UInt8, UInt8, UInt8, UInt8, UInt8, UInt8, UInt8, UInt8, UInt8, UInt8, UInt8, UInt8, UInt8, UInt8, UInt8)
+
+    public var layout: Layout
+
+    init?(layout: Layout) {
+        guard layout.0 == ELFMAG0, /* EI_MAG0 */
+              layout.1 == UInt8(ascii: "E") /* EI_MAG1 */,
+              layout.2 == UInt8(ascii: "L") /* EI_MAG2 */,
+              layout.3 == UInt8(ascii: "F") /* EI_MAG3 */ else {
+            return nil
+        }
+        self.layout = layout
+    }
+}
+
+extension HeaderIdentifier {
+    public var `class`: ELFClass! {
+        .init(rawValue: layout.4) /* EI_CLASS */
+    }
+
+    public var data: ELFData! {
+        .init(rawValue: layout.5) /* EI_DATA */
+    }
+
+    public var version: UInt8 {
+        layout.6 /* EI_VERSION */
+    }
+}
