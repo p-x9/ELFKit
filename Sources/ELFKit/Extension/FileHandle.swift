c1be5d3ccd0219a7da8863fad0dcc92e8add3e3c
p-x9 <50244599+p-x9@users.noreply.github.com>
2024-04-27 03:26:58 +0900

    Add `DataSequence`

--- Sources/ELFKit/Extension/FileHandle.swift
+++ Sources/ELFKit/Extension/FileHandle.swift
@@ -9,6 +9,41 @@
 import Foundation
 
 extension FileHandle {
+    func readDataSequence<Element>(
+        offset: UInt64,
+        numberOfElements: Int,
+        swapHandler: ((inout Data) -> Void)? = nil
+    ) -> DataSequence<Element> where Element: LayoutWrapper {
+        seek(toFileOffset: offset)
+        var data = readData(
+            ofLength: Element.layoutSize * numberOfElements
+        )
+        if let swapHandler { swapHandler(&data) }
+        return .init(
+            data: data,
+            numberOfElements: numberOfElements
+        )
+    }
+
+    @_disfavoredOverload
+    func readDataSequence<Element>(
+        offset: UInt64,
+        numberOfElements: Int,
+        swapHandler: ((inout Data) -> Void)? = nil
+    ) -> DataSequence<Element> {
+        seek(toFileOffset: offset)
+        var data = readData(
+            ofLength: MemoryLayout<Element>.size * numberOfElements
+        )
+        if let swapHandler { swapHandler(&data) }
+        return .init(
+            data: data,
+            numberOfElements: numberOfElements
+        )
+    }
+}
+
+extension FileHandle {
     func read<Element>(
         offset: UInt64,
         swapHandler: ((inout Data) -> Void)? = nil
---
+++ Sources/ELFKit/Util/Sequence/DataSequence.swift
@@ -0,0 +1,91 @@
+//
+//  DataSequence.swift
+//
+//
+//  Created by p-x9 on 2023/12/06.
+//
+//
+
+import Foundation
+
+public struct DataSequence<T>: Sequence {
+    public typealias Element = T
+
+    private let data: Data
+    private let numberOfElements: Int
+
+    init(
+        data: Data,
+        numberOfElements: Int
+    ) {
+        self.data = data
+        self.numberOfElements = numberOfElements
+    }
+
+    public func makeIterator() -> Iterator {
+        Iterator(
+            data: data,
+            numberOfElements: numberOfElements
+        )
+    }
+}
+
+extension DataSequence {
+    public struct Iterator: IteratorProtocol {
+        public typealias Element = T
+
+        private let data: Data
+        private let numberOfElements: Int
+
+        private var nextIndex: Int = 0
+        private var nextOffset: Int = 0
+
+        init(
+            data: Data,
+            numberOfElements: Int
+        ) {
+            self.data = data
+            self.numberOfElements = numberOfElements
+        }
+
+        public mutating func next() -> Element? {
+            guard nextIndex < numberOfElements else { return nil }
+            guard nextOffset < data.count else { return nil }
+
+            defer {
+                nextIndex += 1
+                nextOffset += MemoryLayout<Element>.size
+            }
+
+            return data.withUnsafeBytes {
+                guard let baseAddress = $0.baseAddress else { return nil }
+                return baseAddress.advanced(by: nextOffset).load(as: Element.self)
+            }
+        }
+    }
+}
+
+extension DataSequence: Collection {
+    public typealias Index = Int
+
+    public var startIndex: Index { 0 }
+    public var endIndex: Index { numberOfElements }
+
+    public func index(after i: Int) -> Int {
+        i + 1
+    }
+
+    public subscript(position: Int) -> Element {
+        precondition(position >= 0)
+        precondition(position < endIndex)
+        return data.withUnsafeBytes {
+            guard let baseAddress = $0.baseAddress else { fatalError() }
+            return baseAddress
+                .assumingMemoryBound(to: Element.self)
+                .advanced(by: position)
+                .pointee
+        }
+    }
+}
+
+extension DataSequence: RandomAccessCollection {}
