cacd160eed4cba39900d3e3604923401b006694c
p-x9 <50244599+p-x9@users.noreply.github.com>
2024-04-27 03:25:09 +0900

    Implemented ELF file model

---
+++ Sources/ELFKit/ELFFile.swift
@@ -0,0 +1,54 @@
+//
+//  ELFFile.swift
+//
+//
+//  Created by p-x9 on 2024/04/26
+//
+//
+
+import Foundation
+
+public class ELFFile {
+    /// URL of the file actually loaded
+    public let url: URL
+
+    let fileHandle: FileHandle
+
+    public var is64Bit: Bool { header.identifier.class == ._64 }
+    public var headerSize: Int {
+        is64Bit ? ELF64Header.layoutSize : ELF32Header.layoutSize
+    }
+
+    public let header: ELFHeader
+
+    init(url: URL) throws {
+        self.url = url
+        self.fileHandle = try FileHandle(forReadingFrom: url)
+
+        let identifier: HeaderIdentifier = fileHandle.read(
+            offset: 0
+        )
+
+        let header: ELFHeader
+        switch identifier.class {
+        case ._32:
+            let _header: ELF32Header = fileHandle.read(
+                offset: 0
+            )
+            header = ._32(_header)
+        case ._64:
+            let _header: ELF64Header = fileHandle.read(
+                offset: 0
+            )
+            header = ._64(_header)
+        default:
+            throw NSError() // FIXME: Invalid ELF class
+        }
+
+        self.header = header
+    }
+
+    deinit {
+        fileHandle.closeFile()
+    }
+}
