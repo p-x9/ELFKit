f8df829628619dc25c3a4fa69b76cbad8103f3e8
p-x9 <50244599+p-x9@users.noreply.github.com>
2024-04-30 09:25:07 +0900

    Add `nameOffset`

--- Sources/ELFKit/Protocol/ELFVersionDefAuxProtocol.swift
+++ Sources/ELFKit/Protocol/ELFVersionDefAuxProtocol.swift
@@ -11,6 +11,7 @@ import Foundation
 public protocol ELFVersionDefAuxProtocol {
     var _offset: Int { get }
     var _index: Int { get }
+    var nameOffset: Int { get }
     var nextOffset: Int { get }
 
     func next(in elf: ELFFile) -> Self?
