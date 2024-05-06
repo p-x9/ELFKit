9d8d0584a667f25d45d24726a3a82be4d71871b3
p-x9 <50244599+p-x9@users.noreply.github.com>
2024-05-06 18:24:41 +0900

    Implemented function to find symbols with gnu hash table

--- Sources/ELFKit/Protocol/ELFHashTableProtocol.swift
+++ Sources/ELFKit/Protocol/ELFHashTableProtocol.swift
@@ -59,3 +59,118 @@ extension ELFGnuHashTableProtocol {
         return numericCast(ix) + 1 // First `STN_UNDEF` symbol
     }
 }
+
+extension ELFGnuHashTableProtocol {
+    public static func hash(for name: String) -> Int {
+        let cString = name.cString(using: .utf8)
+        var h: UInt = 5381
+
+        if let cString {
+            for ch in cString where ch != 0 {
+                h = h &* 33 + UInt(ch)
+            }
+        }
+
+        return numericCast(h & 0xffffffff)
+    }
+}
+
+extension ELFGnuHashTableProtocol {
+    public func findSymbol64(
+        named symbol: String,
+        in elf: ELFFile
+    ) -> ELF64Symbol? {
+        guard let dynamics = elf.dynamics64,
+              let hashTable = dynamics.gnuHashTable(in: elf),
+              let symbols = dynamics.symbols(in: elf) else {
+            return nil
+        }
+        let header = hashTable.header
+        let bits = 64
+
+        let hash = Self.hash(for: symbol)
+
+        let word = hashTable.bloom[(hash / bits) % numericCast(header.gh_maskwords)]
+        let mask: UInt64 = 0 | 1 << (hash % bits) | 1 << ((hash >> UInt64(header.gh_shift2)) % bits)
+
+        if (word & mask) != mask {
+            // Symbol Not Found"
+            return nil
+        }
+
+        var symix = hashTable.buckets[hash % numericCast(header.gh_nbuckets)]
+        if symix < numericCast(header.gh_symndx) {
+            // Symbol Not Found
+            return nil
+        }
+
+        while true {
+            let current = symbols[Int(symix)]
+            let name = current.name(in: elf, isDynamic: true)
+            let nhash: UInt32 = elf.fileHandle.read(
+                offset: numericCast( hashTable.chainsOffset) + numericCast(MemoryLayout<Hashelt>.size) * numericCast(symix - header.gh_symndx))
+            if (hash|1) == (nhash|1) && name == symbol {
+               return current
+            }
+            if (hash & 1) != 0 { break }
+            symix += 1
+        }
+
+        return nil
+    }
+
+    public func findSymbol32(
+        named symbol: String,
+        in elf: ELFFile
+    ) -> ELF32Symbol? {
+        guard let dynamics = elf.dynamics32,
+              let hashTable = dynamics.gnuHashTable(in: elf),
+              let symbols = dynamics.symbols(in: elf) else {
+            return nil
+        }
+        let header = hashTable.header
+        let bits = 32
+
+        let hash = Self.hash(for: symbol)
+
+        let word = hashTable.bloom[(hash / bits) % numericCast(header.gh_maskwords)]
+        let mask: UInt32 = 0 | 1 << (hash % bits) | 1 << ((hash >> UInt32(header.gh_shift2)) % bits)
+
+        if (word & mask) != mask {
+            // Symbol Not Found"
+            return nil
+        }
+
+        var symix = hashTable.buckets[hash % numericCast(header.gh_nbuckets)]
+        if symix < numericCast(header.gh_symndx) {
+            // Symbol Not Found
+            return nil
+        }
+
+        while true {
+            let current = symbols[Int(symix)]
+            let name = current.name(in: elf, isDynamic: true)
+            let nhash: UInt32 = elf.fileHandle.read(
+                offset: numericCast( hashTable.chainsOffset) + numericCast(MemoryLayout<Hashelt>.size) * numericCast(symix - header.gh_symndx))
+            if (hash|1) == (nhash|1) && name == symbol {
+                return current
+            }
+            if (hash & 1) != 0 { break }
+            symix += 1
+        }
+
+        return nil
+    }
+
+    public func findSymbol(
+        named symbol: String,
+        in elf: ELFFile
+    ) -> (any ELFSymbolProtocol)? {
+        if elf.is64Bit {
+            findSymbol64(named: symbol, in: elf)
+        } else {
+            findSymbol32(named: symbol, in: elf)
+        }
+    }
+}
+
