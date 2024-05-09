5a8abb38ee3c463252e8f2d09f28f7061cf31272
p-x9 <50244599+p-x9@users.noreply.github.com>
2024-05-09 23:05:38 +0900

    Add new flags to `DynamicFlags1`

--- Sources/ELFKit/Model/Dynamic/DynamicFlags1.swift
+++ Sources/ELFKit/Model/Dynamic/DynamicFlags1.swift
@@ -56,6 +56,10 @@ extension DynamicFlags1 {
     public static let direct = DynamicFlags1(
         rawValue: Bit.direct.rawValue
     )
+    /// DF_1_TRANS
+    public static let trans = DynamicFlags1(
+        rawValue: Bit.trans.rawValue
+    )
     /// DF_1_INTERPOSE
     public static let interpose = DynamicFlags1(
         rawValue: Bit.interpose.rawValue
@@ -128,136 +132,86 @@ extension DynamicFlags1 {
     public static let pie = DynamicFlags1(
         rawValue: Bit.pie.rawValue
     )
+    /// DF_1_KMOD
+    public static let kmod = DynamicFlags1(
+        rawValue: Bit.kmod.rawValue
+    )
+    /// DF_1_WEAKFILTER
+    public static let weakfilter = DynamicFlags1(
+        rawValue: Bit.weakfilter.rawValue
+    )
+    /// DF_1_NOCOMMON
+    public static let nocommon = DynamicFlags1(
+        rawValue: Bit.nocommon.rawValue
+    )
 }
 
 extension DynamicFlags1 {
-    public enum Bit: CaseIterable {
+    public enum Bit: UInt32, CaseIterable {
         /// DF_1_NOW
-        case now
+        case now = 1
         /// DF_1_GLOBAL
-        case global
+        case global = 2
         /// DF_1_GROUP
-        case group
+        case group = 4
         /// DF_1_NODELETE
-        case nodelete
+        case nodelete = 8
         /// DF_1_LOADFLTR
-        case loadfltr
+        case loadfltr = 0x10
         /// DF_1_INITFIRST
-        case initfirst
+        case initfirst = 0x20
         /// DF_1_NOOPEN
-        case noopen
+        case noopen = 0x40
         /// DF_1_ORIGIN
-        case origin
+        case origin = 0x80
         /// DF_1_DIRECT
-        case direct
+        case direct = 0x100
+        /// DF_1_TRANS
+        case trans = 0x200
         /// DF_1_INTERPOSE
-        case interpose
+        case interpose = 0x400
         /// DF_1_NODEFLIB
-        case nodeflib
+        case nodeflib = 0x800
         /// DF_1_NODUMP
-        case nodump
+        case nodump = 0x1000
         /// DF_1_CONFALT
-        case confalt
+        case confalt = 0x2000
         /// DF_1_ENDFILTEE
-        case endfiltee
+        case endfiltee = 0x4000
         /// DF_1_DISPRELDNE
-        case dispreldne
+        case dispreldne = 0x8000
         /// DF_1_DISPRELPND
-        case disprelpnd
+        case disprelpnd = 0x10000
         /// DF_1_NODIRECT
-        case nodirect
+        case nodirect = 0x20000
         /// DF_1_IGNMULDEF
-        case ignmuldef
+        case ignmuldef = 0x40000
         /// DF_1_NOKSYMS
-        case noksyms
+        case noksyms = 0x80000
         /// DF_1_NOHDR
-        case nohdr
+        case nohdr = 0x100000
         /// DF_1_EDITED
-        case edited
+        case edited = 0x200000
         /// DF_1_NORELOC
-        case noreloc
+        case noreloc = 0x400000
         /// DF_1_SYMINTPOSE
-        case symintpose
+        case symintpose = 0x800000
         /// DF_1_GLOBAUDIT
-        case globaudit
+        case globaudit = 0x1000000
         /// DF_1_SINGLETON
-        case singleton
+        case singleton = 0x2000000
         /// DF_1_STUB
-        case stub
+        case stub = 0x4000000
         /// DF_1_PIE
-        case pie
+        case pie = 0x8000000
+        /// DF_1_KMOD
+        case kmod = 0x10000000
+        /// DF_1_WEAKFILTER
+        case weakfilter = 0x20000000
+        /// DF_1_NOCOMMON
+        case nocommon = 0x40000000
     }
 }
-
-extension DynamicFlags1.Bit: RawRepresentable {
-    public typealias RawValue = UInt32
-
-    public init?(rawValue: RawValue) {
-        switch rawValue {
-        case RawValue(DF_1_NOW): self = .now
-        case RawValue(DF_1_GLOBAL): self = .global
-        case RawValue(DF_1_GROUP): self = .group
-        case RawValue(DF_1_NODELETE): self = .nodelete
-        case RawValue(DF_1_LOADFLTR): self = .loadfltr
-        case RawValue(DF_1_INITFIRST): self = .initfirst
-        case RawValue(DF_1_NOOPEN): self = .noopen
-        case RawValue(DF_1_ORIGIN): self = .origin
-        case RawValue(DF_1_DIRECT): self = .direct
-        case RawValue(DF_1_INTERPOSE): self = .interpose
-        case RawValue(DF_1_NODEFLIB): self = .nodeflib
-        case RawValue(DF_1_NODUMP): self = .nodump
-        case RawValue(DF_1_CONFALT): self = .confalt
-        case RawValue(DF_1_ENDFILTEE): self = .endfiltee
-        case RawValue(DF_1_DISPRELDNE): self = .dispreldne
-        case RawValue(DF_1_DISPRELPND): self = .disprelpnd
-        case RawValue(DF_1_NODIRECT): self = .nodirect
-        case RawValue(DF_1_IGNMULDEF): self = .ignmuldef
-        case RawValue(DF_1_NOKSYMS): self = .noksyms
-        case RawValue(DF_1_NOHDR): self = .nohdr
-        case RawValue(DF_1_EDITED): self = .edited
-        case RawValue(DF_1_NORELOC): self = .noreloc
-        case RawValue(DF_1_SYMINTPOSE): self = .symintpose
-        case RawValue(DF_1_GLOBAUDIT): self = .globaudit
-        case RawValue(DF_1_SINGLETON): self = .singleton
-        case RawValue(DF_1_STUB): self = .stub
-        case RawValue(DF_1_PIE): self = .pie
-        default: return nil
-        }
-    }
-
-    public var rawValue: RawValue {
-        switch self {
-        case .now: RawValue(DF_1_NOW)
-        case .global: RawValue(DF_1_GLOBAL)
-        case .group: RawValue(DF_1_GROUP)
-        case .nodelete: RawValue(DF_1_NODELETE)
-        case .loadfltr: RawValue(DF_1_LOADFLTR)
-        case .initfirst: RawValue(DF_1_INITFIRST)
-        case .noopen: RawValue(DF_1_NOOPEN)
-        case .origin: RawValue(DF_1_ORIGIN)
-        case .direct: RawValue(DF_1_DIRECT)
-        case .interpose: RawValue(DF_1_INTERPOSE)
-        case .nodeflib: RawValue(DF_1_NODEFLIB)
-        case .nodump: RawValue(DF_1_NODUMP)
-        case .confalt: RawValue(DF_1_CONFALT)
-        case .endfiltee: RawValue(DF_1_ENDFILTEE)
-        case .dispreldne: RawValue(DF_1_DISPRELDNE)
-        case .disprelpnd: RawValue(DF_1_DISPRELPND)
-        case .nodirect: RawValue(DF_1_NODIRECT)
-        case .ignmuldef: RawValue(DF_1_IGNMULDEF)
-        case .noksyms: RawValue(DF_1_NOKSYMS)
-        case .nohdr: RawValue(DF_1_NOHDR)
-        case .edited: RawValue(DF_1_EDITED)
-        case .noreloc: RawValue(DF_1_NORELOC)
-        case .symintpose: RawValue(DF_1_SYMINTPOSE)
-        case .globaudit: RawValue(DF_1_GLOBAUDIT)
-        case .singleton: RawValue(DF_1_SINGLETON)
-        case .stub: RawValue(DF_1_STUB)
-        case .pie: RawValue(DF_1_PIE)
-        }
-    }
-}
-
 
 extension DynamicFlags1.Bit: CustomStringConvertible {
     public var description: String {
@@ -271,6 +225,7 @@ extension DynamicFlags1.Bit: CustomStringConvertible {
         case .noopen: "DF_1_NOOPEN"
         case .origin: "DF_1_ORIGIN"
         case .direct: "DF_1_DIRECT"
+        case .trans: "DF_1_TRANS"
         case .interpose: "DF_1_INTERPOSE"
         case .nodeflib: "DF_1_NODEFLIB"
         case .nodump: "DF_1_NODUMP"
@@ -289,6 +244,9 @@ extension DynamicFlags1.Bit: CustomStringConvertible {
         case .singleton: "DF_1_SINGLETON"
         case .stub: "DF_1_STUB"
         case .pie: "DF_1_PIE"
+        case .kmod: "DF_1_KMOD"
+        case .weakfilter: "DF_1_WEAKFILTER"
+        case .nocommon: "DF_1_NOCOMMON"
         }
     }
 }
