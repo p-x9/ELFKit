e8caf9af00cee80d4584e17ef21aa0d75dd1e654
p-x9 <50244599+p-x9@users.noreply.github.com>
2024-05-09 22:56:53 +0900

    Update ELFMachine

--- Sources/ELFKit/Header/Model/ELFMachine.swift
+++ Sources/ELFKit/Header/Model/ELFMachine.swift
@@ -32,10 +32,14 @@ public enum ELFMachine: UInt16 {
     case s370 = 9
     /// EM_MIPS_RS3_LE
     case mips_rs3_le = 10
+    /// EM_OLD_SPARCV9
+    case old_sparcv9 = 11
     /// EM_PARISC
     case parisc = 15
-    /// EM_VPP500
-    case vpp500 = 17
+//    /// EM_PPC_OLD
+//    case ppc_old = 17
+    /// EM_VPP550
+    case vpp550 = 17
     /// EM_SPARC32PLUS
     case sparc32plus = 18
     /// EM_960
@@ -54,12 +58,14 @@ public enum ELFMachine: UInt16 {
     case fr20 = 37
     /// EM_RH32
     case rh32 = 38
-    /// EM_RCE
-    case rce = 39
+    /// EM_MCORE
+    case mcore = 39
+//    /// EM_RCE
+//    case rce = 39
     /// EM_ARM
     case arm = 40
-    /// EM_FAKE_ALPHA
-    case fake_alpha = 41
+    /// EM_OLD_ALPHA
+    case old_alpha = 41
     /// EM_SH
     case sh = 42
     /// EM_SPARCV9
@@ -166,6 +172,8 @@ public enum ELFMachine: UInt16 {
     case arc_compact = 93
     /// EM_XTENSA
     case xtensa = 94
+//    /// EM_SCORE_OLD
+//    case score_old = 95
     /// EM_VIDEOCORE
     case videocore = 95
     /// EM_TMM_GPP
@@ -174,6 +182,8 @@ public enum ELFMachine: UInt16 {
     case ns32k = 97
     /// EM_TPC
     case tpc = 98
+//    /// EM_PJ_OLD
+//    case pj_old = 99
     /// EM_SNP1K
     case snp1k = 99
     /// EM_ST200
@@ -206,6 +216,8 @@ public enum ELFMachine: UInt16 {
     case altera_nios2 = 113
     /// EM_CRX
     case crx = 114
+//    /// EM_CR16_OLD
+//    case cr16_old = 115
     /// EM_XGATE
     case xgate = 115
     /// EM_C166
@@ -226,8 +238,10 @@ public enum ELFMachine: UInt16 {
     case sharc = 133
     /// EM_ECOG2
     case ecog2 = 134
-    /// EM_SCORE7
-    case score7 = 135
+    /// EM_SCORE
+    case score = 135
+//    /// EM_SCORE7
+//    case score7 = 135
     /// EM_DSP24
     case dsp24 = 136
     /// EM_VIDEOCORE3
@@ -262,8 +276,10 @@ public enum ELFMachine: UInt16 {
     case stxp7x = 166
     /// EM_NDS32
     case nds32 = 167
-    /// EM_ECOG1X
-    case ecog1x = 168
+    /// EM_ECOG1
+    case ecog1 = 168
+//    /// EM_ECOG1X
+//    case ecog1x = 168
     /// EM_MAXQ30
     case maxq30 = 169
     /// EM_XIMO16
@@ -290,8 +306,12 @@ public enum ELFMachine: UInt16 {
     case l10m = 180
     /// EM_K10M
     case k10m = 181
+    /// EM_INTEL182
+    case intel182 = 182
     /// EM_AARCH64
     case aarch64 = 183
+    /// EM_ARM184
+    case arm184 = 184
     /// EM_AVR32
     case avr32 = 185
     /// EM_STM8
@@ -312,8 +332,8 @@ public enum ELFMachine: UInt16 {
     case corea_1st = 193
     /// EM_COREA_2ND
     case corea_2nd = 194
-    /// EM_ARCV2
-    case arcv2 = 195
+    /// EM_ARC_COMPACT2
+    case arc_compact2 = 195
     /// EM_OPEN8
     case open8 = 196
     /// EM_RL78
@@ -334,6 +354,14 @@ public enum ELFMachine: UInt16 {
     case mchp_pic = 204
     /// EM_INTELGT
     case intelgt = 205
+    /// EM_INTEL206
+    case intel206 = 206
+    /// EM_INTEL207
+    case intel207 = 207
+    /// EM_INTEL208
+    case intel208 = 208
+    /// EM_INTEL209
+    case intel209 = 209
     /// EM_KM32
     case km32 = 210
     /// EM_KMX32
@@ -366,14 +394,100 @@ public enum ELFMachine: UInt16 {
     case amdgpu = 224
     /// EM_RISCV
     case riscv = 243
+    /// EM_LANAI
+    case lanai = 244
+    /// EM_CEVA
+    case ceva = 245
+    /// EM_CEVA_X2
+    case ceva_x2 = 246
     /// EM_BPF
     case bpf = 247
+    /// EM_GRAPHCORE_IPU
+    case graphcore_ipu = 248
+    /// EM_IMG1
+    case img1 = 249
+    /// EM_NFP
+    case nfp = 250
+    /// EM_VE
+    case ve = 251
     /// EM_CSKY
     case csky = 252
+    /// EM_ARC_COMPACT3_64
+    case arc_compact3_64 = 253
+    /// EM_MCS6502
+    case mcs6502 = 254
+    /// EM_ARC_COMPACT3
+    case arc_compact3 = 0xff
+    /// EM_KVX
+    case kvx = 0x100
+    /// EM_65816
+    case _65816 = 0x101
     /// EM_LOONGARCH
     case loongarch = 0x102
+    /// EM_KF32
+    case kf32 = 0x103
+    /// EM_U16_U8CORE
+    case u16_u8core = 0x104
+    /// EM_TACHYUM
+    case tachyum = 0x105
+    /// EM_56800EF
+    case _56800ef = 0x106
+    /// EM_AVR_OLD
+    case avr_old = 0x1057
+    /// EM_MSP430_OLD
+    case msp430_old = 0x1059
+    /// EM_MT
+    case mt = 0x2530
+    /// EM_CYGNUS_FR30
+    case cygnus_fr30 = 0x3330
+    /// EM_WEBASSEMBLY
+    case webassembly = 0x4157
+    /// EM_S12Z
+    case s12z = 0x4def
+    /// EM_DLX
+    case dlx = 0x5aa5
+    /// EM_CYGNUS_FRV
+    case cygnus_frv = 0x5441
+    /// EM_XC16X
+    case xc16x = 0x4688
+    /// EM_CYGNUS_D10V
+    case cygnus_d10v = 0x7650
+    /// EM_CYGNUS_D30V
+    case cygnus_d30v = 0x7676
+    /// EM_IP2K_OLD
+    case ip2k_old = 0x8217
+    /// EM_CYGNUS_POWERPC
+    case cygnus_powerpc = 0x9025
     /// EM_ALPHA
     case alpha = 0x9026
+    /// EM_CYGNUS_M32R
+    case cygnus_m32r = 0x9041
+    /// EM_CYGNUS_V850
+    case cygnus_v850 = 0x9080
+    /// EM_S390_OLD
+    case s390_old = 0xa390
+    /// EM_XTENSA_OLD
+    case xtensa_old = 0xabc7
+    /// EM_XSTORMY16
+    case xstormy16 = 0xad45
+    /// EM_CYGNUS_MN10300
+    case cygnus_mn10300 = 0xbeef
+    /// EM_CYGNUS_MN10200
+    case cygnus_mn10200 = 0xdead
+    /// EM_M32C_OLD
+    case m32c_old = 0xfeb0
+    /// EM_IQ2000
+    case iq2000 = 0xfeba
+    /// EM_NIOS32
+    case nios32 = 0xfebb
+    /// EM_CYGNUS_MEP
+    case cygnus_mep = 0xf00d
+    /// EM_MOXIE_OLD
+    case moxie_old = 0xfeed
+    /// EM_MICROBLAZE_OLD
+    case microblaze_old = 0xbaab
+    /// EM_ADAPTEVA_EPIPHANY
+    case adapteva_epiphany = 0x1223
 }
 
 extension ELFMachine: CustomStringConvertible {
@@ -390,8 +504,10 @@ extension ELFMachine: CustomStringConvertible {
         case .mips: "EM_MIPS"
         case .s370: "EM_S370"
         case .mips_rs3_le: "EM_MIPS_RS3_LE"
+        case .old_sparcv9: "EM_OLD_SPARCV9"
         case .parisc: "EM_PARISC"
-        case .vpp500: "EM_VPP500"
+//        case .ppc_old: "EM_PPC_OLD"
+        case .vpp550: "EM_VPP550"
         case .sparc32plus: "EM_SPARC32PLUS"
         case ._960: "EM_960"
         case .ppc: "EM_PPC"
@@ -401,9 +517,10 @@ extension ELFMachine: CustomStringConvertible {
         case .v800: "EM_V800"
         case .fr20: "EM_FR20"
         case .rh32: "EM_RH32"
-        case .rce: "EM_RCE"
+        case .mcore: "EM_MCORE"
+//        case .rce: "EM_RCE"
         case .arm: "EM_ARM"
-        case .fake_alpha: "EM_FAKE_ALPHA"
+        case .old_alpha: "EM_OLD_ALPHA"
         case .sh: "EM_SH"
         case .sparcv9: "EM_SPARCV9"
         case .tricore: "EM_TRICORE"
@@ -457,10 +574,12 @@ extension ELFMachine: CustomStringConvertible {
         case .openrisc: "EM_OPENRISC"
         case .arc_compact: "EM_ARC_COMPACT"
         case .xtensa: "EM_XTENSA"
+//        case .score_old: "EM_SCORE_OLD"
         case .videocore: "EM_VIDEOCORE"
         case .tmm_gpp: "EM_TMM_GPP"
         case .ns32k: "EM_NS32K"
         case .tpc: "EM_TPC"
+//        case .pj_old: "EM_PJ_OLD"
         case .snp1k: "EM_SNP1K"
         case .st200: "EM_ST200"
         case .ip2k: "EM_IP2K"
@@ -477,6 +596,7 @@ extension ELFMachine: CustomStringConvertible {
         case .dxp: "EM_DXP"
         case .altera_nios2: "EM_ALTERA_NIOS2"
         case .crx: "EM_CRX"
+//        case .cr16_old: "EM_CR16_OLD"
         case .xgate: "EM_XGATE"
         case .c166: "EM_C166"
         case .m16c: "EM_M16C"
@@ -487,7 +607,8 @@ extension ELFMachine: CustomStringConvertible {
         case .rs08: "EM_RS08"
         case .sharc: "EM_SHARC"
         case .ecog2: "EM_ECOG2"
-        case .score7: "EM_SCORE7"
+        case .score: "EM_SCORE"
+//        case .score7: "EM_SCORE7"
         case .dsp24: "EM_DSP24"
         case .videocore3: "EM_VIDEOCORE3"
         case .latticemico32: "EM_LATTICEMICO32"
@@ -505,7 +626,8 @@ extension ELFMachine: CustomStringConvertible {
         case ._8051: "EM_8051"
         case .stxp7x: "EM_STXP7X"
         case .nds32: "EM_NDS32"
-        case .ecog1x: "EM_ECOG1X"
+        case .ecog1: "EM_ECOG1"
+//        case .ecog1x: "EM_ECOG1X"
         case .maxq30: "EM_MAXQ30"
         case .ximo16: "EM_XIMO16"
         case .manik: "EM_MANIK"
@@ -519,7 +641,9 @@ extension ELFMachine: CustomStringConvertible {
         case .sle9x: "EM_SLE9X"
         case .l10m: "EM_L10M"
         case .k10m: "EM_K10M"
+        case .intel182: "EM_INTEL182"
         case .aarch64: "EM_AARCH64"
+        case .arm184: "EM_ARM184"
         case .avr32: "EM_AVR32"
         case .stm8: "EM_STM8"
         case .tile64: "EM_TILE64"
@@ -530,7 +654,7 @@ extension ELFMachine: CustomStringConvertible {
         case .cloudshield: "EM_CLOUDSHIELD"
         case .corea_1st: "EM_COREA_1ST"
         case .corea_2nd: "EM_COREA_2ND"
-        case .arcv2: "EM_ARCV2"
+        case .arc_compact2: "EM_ARC_COMPACT2"
         case .open8: "EM_OPEN8"
         case .rl78: "EM_RL78"
         case .videocore5: "EM_VIDEOCORE5"
@@ -541,6 +665,10 @@ extension ELFMachine: CustomStringConvertible {
         case .xcore: "EM_XCORE"
         case .mchp_pic: "EM_MCHP_PIC"
         case .intelgt: "EM_INTELGT"
+        case .intel206: "EM_INTEL206"
+        case .intel207: "EM_INTEL207"
+        case .intel208: "EM_INTEL208"
+        case .intel209: "EM_INTEL209"
         case .km32: "EM_KM32"
         case .kmx32: "EM_KMX32"
         case .emx16: "EM_EMX16"
@@ -557,10 +685,53 @@ extension ELFMachine: CustomStringConvertible {
         case .moxie: "EM_MOXIE"
         case .amdgpu: "EM_AMDGPU"
         case .riscv: "EM_RISCV"
+        case .lanai: "EM_LANAI"
+        case .ceva: "EM_CEVA"
+        case .ceva_x2: "EM_CEVA_X2"
         case .bpf: "EM_BPF"
+        case .graphcore_ipu: "EM_GRAPHCORE_IPU"
+        case .img1: "EM_IMG1"
+        case .nfp: "EM_NFP"
+        case .ve: "EM_VE"
         case .csky: "EM_CSKY"
+        case .arc_compact3_64: "EM_ARC_COMPACT3_64"
+        case .mcs6502: "EM_MCS6502"
+        case .arc_compact3: "EM_ARC_COMPACT3"
+        case .kvx: "EM_KVX"
+        case ._65816: "EM_65816"
         case .loongarch: "EM_LOONGARCH"
+        case .kf32: "EM_KF32"
+        case .u16_u8core: "EM_U16_U8CORE"
+        case .tachyum: "EM_TACHYUM"
+        case ._56800ef: "EM_56800EF"
+        case .avr_old: "EM_AVR_OLD"
+        case .msp430_old: "EM_MSP430_OLD"
+        case .mt: "EM_MT"
+        case .cygnus_fr30: "EM_CYGNUS_FR30"
+        case .webassembly: "EM_WEBASSEMBLY"
+        case .s12z: "EM_S12Z"
+        case .dlx: "EM_DLX"
+        case .cygnus_frv: "EM_CYGNUS_FRV"
+        case .xc16x: "EM_XC16X"
+        case .cygnus_d10v: "EM_CYGNUS_D10V"
+        case .cygnus_d30v: "EM_CYGNUS_D30V"
+        case .ip2k_old: "EM_IP2K_OLD"
+        case .cygnus_powerpc: "EM_CYGNUS_POWERPC"
         case .alpha: "EM_ALPHA"
+        case .cygnus_m32r: "EM_CYGNUS_M32R"
+        case .cygnus_v850: "EM_CYGNUS_V850"
+        case .s390_old: "EM_S390_OLD"
+        case .xtensa_old: "EM_XTENSA_OLD"
+        case .xstormy16: "EM_XSTORMY16"
+        case .cygnus_mn10300: "EM_CYGNUS_MN10300"
+        case .cygnus_mn10200: "EM_CYGNUS_MN10200"
+        case .m32c_old: "EM_M32C_OLD"
+        case .iq2000: "EM_IQ2000"
+        case .nios32: "EM_NIOS32"
+        case .cygnus_mep: "EM_CYGNUS_MEP"
+        case .moxie_old: "EM_MOXIE_OLD"
+        case .microblaze_old: "EM_MICROBLAZE_OLD"
+        case .adapteva_epiphany: "EM_ADAPTEVA_EPIPHANY"
         }
     }
 }
