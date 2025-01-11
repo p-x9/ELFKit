b00ab39a5f3a3ea99239c6f749a6129f0c0b26ba
p-x9 <50244599+p-x9@users.noreply.github.com>
2025-01-12 03:22:59 +0900

    Fix conversion of char tuple to string

--- Sources/ELFKit/Extension/String+.swift
+++ Sources/ELFKit/Extension/String+.swift
@@ -12,10 +12,10 @@ extension String {
     typealias UInt8Tuple16 = (UInt8, UInt8, UInt8, UInt8, UInt8, UInt8, UInt8, UInt8, UInt8, UInt8, UInt8, UInt8, UInt8, UInt8, UInt8, UInt8)
 
     init(tuple: UInt8Tuple16) {
-        var buffer = tuple
-        self = withUnsafePointer(to: &buffer.0) {
-            let data = Data(bytes: $0, count: 16) + [0]
-            return String(cString: data)!
+        self = withUnsafePointer(to: tuple) {
+            let size = MemoryLayout<UInt8Tuple16>.size
+            let data = Data(bytes: $0, count: size) + [0]
+            return String(cString: data) ?? ""
         }
     }
 }
