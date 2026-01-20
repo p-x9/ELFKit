ac0f8012acc9d8f1fd78a42a8ad493c4b80d5986
p-x9 <50244599+p-x9@users.noreply.github.com>
2026-01-21 02:35:41 +0900

    Add packages for binary parsing to dependencies

--- Package.swift
+++ Package.swift
@@ -11,14 +11,27 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/p-x9/swift-fileio.git", from: "0.9.0")
+        .package(
+            url: "https://github.com/p-x9/swift-binary-parse-support.git",
+            from: "0.2.0"
+        ),
+        .package(
+            url: "https://github.com/p-x9/swift-fileio.git",
+            from: "0.9.0"
+        ),
+        .package(
+            url: "https://github.com/p-x9/swift-fileio-extra.git",
+            from: "0.2.0"
+        ),
     ],
     targets: [
         .target(
             name: "ELFKit",
             dependencies: [
                 "ELFKitC",
-                .product(name: "FileIO", package: "swift-fileio")
+                .product(name: "BinaryParseSupport", package: "swift-binary-parse-support"),
+                .product(name: "FileIO", package: "swift-fileio"),
+                .product(name: "FileIOBinary", package: "swift-fileio-extra")
             ]
         ),
         .target(
